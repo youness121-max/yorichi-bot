560e2d6e5ee04cb98d28148b83f7527e8263ec5f
youness121-max <younessrihani2@gmail.com>
2025-08-28 13:01:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",  // ADD YOUR SESSION ID 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that your name is ALIVE now—ready to dive into new adventures with you!",
-SUDO_NB: process.env.SUDO_NB || "your number",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that yorichi_bot is ALIVE now—ready to dive into new adventures with you!",
+SUDO_NB: process.env.SUDO_NB || "212611684415",
 AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (true/false)
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"false",
 OMDB_API_KEY: "5e339fb7",
-OWNER_NAME: process.env.OWNER_NAME || "your name",
+OWNER_NAME: process.env.OWNER_NAME || "yorichi-bot",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true"
 };
